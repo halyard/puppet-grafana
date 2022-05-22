99575d67a50c336ed0b24ae3faef0b786482b667
Les Aker <me@lesaker.org>
2022-05-22 12:48:01 -0400

    add template

--- manifests/init.pp
+++ manifests/init.pp
@@ -3,11 +3,21 @@
 # @param hostname sets the hostname for grafana
 # @param datadir sets where the data is persisted
 # @param tls_account sets the TLS account config
+# @param admin_user sets the username for the primary Grafana account
+# @param admin_password sets the password for the primary Grafana account
+# @param secret_key sets the AES key used for encrypting Grafana sessions
+# @param client_id sets the Github OAuth client ID
+# @param client_secret sets the Github OAuth client secret
 # @param tls_challengealias sets the alias for TLS cert
 class grafana (
   String $hostname,
   String $datadir,
   String $tls_account,
+  String $admin_user,
+  String $admin_password,
+  String $secret_key,
+  String $client_id,
+  String $client_secret,
   Optional[String] $tls_challengealias = undef,
 ) {
   file { ["${datadir}/data", "${datadir}/provisioning", "${datadir}/certs"]:
